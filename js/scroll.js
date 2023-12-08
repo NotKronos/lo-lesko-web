6968acd971d1f7e7fe364206572e1de2ff768756
C10H15NO <C10H15NO@teczowysekai.live>
2023-12-08 14:24:05 +0100

    added a progress bar to the article track

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 document.addEventListener('wheel', scroll);
 let prevScroll = 0;
-
+const maxScroll = -47.5;
 function scroll(e) {
 
     const maxDelta = window.innerWidth * 0.5;
@@ -11,10 +11,13 @@ function scroll(e) {
         return;
     } else if(percent < -47.5) {
         document.getElementById('article-track').style.transform = `translate(-47.5%, -50%)`;
+        document.getElementById('scroll-progress').style.width = `100%`;
         return;
     } else {
         prevScroll = percent;
         document.getElementById('article-track').style.transform = `translate(${percent}%, -50%)`;
     }
+    const scrollProgress = percent / maxScroll * 100;
+    document.getElementById('scroll-progress').style.width = `${scrollProgress}%`;
 }
 
